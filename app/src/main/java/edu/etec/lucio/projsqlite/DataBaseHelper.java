d9e3486185ed746499a2bc18017f219fec93061a
MysteryMS <piva.pedro@outlook.com>
2023-03-10 14:45:52 -0300

    criando tabelas

--- app/src/main/java/edu/etec/lucio/projsqlite/DataBaseHelper.java
+++ app/src/main/java/edu/etec/lucio/projsqlite/DataBaseHelper.java
@@ -6,8 +6,11 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
 
-public class DataBaseHelper extends SQLiteOpenHelper
-{
+public class DataBaseHelper extends SQLiteOpenHelper {
+
+    public static final String TABELAUSUARIO = "usuario";
+    public static final String NOMEUSUARIO = "nome";
+    public static final String SENHAUSUARIO = "senha";
 
     public DataBaseHelper(@Nullable Context context) {
         super(context, "bike.db", null, 1);
@@ -15,7 +18,7 @@ public class DataBaseHelper extends SQLiteOpenHelper
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-
+        String criaTabela = "CREATE TABLE " + TABELAUSUARIO + " (" + NOMEUSUARIO + " TEXT " + SENHAUSUARIO + " TEXT)";
 
     }
 
