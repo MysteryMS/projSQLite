84292170800c39c98c6e92bf5258e23240daadc8
MysteryMS <piva.pedro@outlook.com>
2023-03-10 14:29:09 -0300

    first commit

---
+++ app/src/main/java/edu/etec/lucio/projsqlite/DataBaseHelper.java
@@ -0,0 +1,26 @@
+package edu.etec.lucio.projsqlite;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DataBaseHelper extends SQLiteOpenHelper
+{
+
+    public DataBaseHelper(@Nullable Context context) {
+        super(context, "bike.db", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
---
+++ app/src/main/java/edu/etec/lucio/projsqlite/UsuarioModel.java
@@ -0,0 +1,50 @@
+package edu.etec.lucio.projsqlite;
+
+public class UsuarioModel {
+
+    private int id;
+    private String nome;
+    private String senha;
+
+    public UsuarioModel() {
+    }
+
+    public UsuarioModel(int id, String nome, String senha) {
+        this.id = id;
+        this.nome = nome;
+        this.senha = senha;
+    }
+
+    @Override
+    public String toString() {
+        return "UsuarioModel{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", senha='" + senha + '\'' +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
